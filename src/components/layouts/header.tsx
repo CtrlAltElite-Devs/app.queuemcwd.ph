4ca05f21ff26f517991f7115599616a68899e074
ShinjiCodeEVA <solisharvie1@gmail.com>
2025-10-28 16:20:50 +0800

    refactor: apply layout structure

---
+++ src/components/layouts/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="w-full bg-black px-6 py-4 shadow-sm dark:bg-zinc-900">
+      <h1 className="text-center text-lg font-semibold text-white sm:text-left sm:text-xl md:text-2xl">
+        Metropolitan Cebu Water District
+      </h1>
+    </header>
+  );
+}
