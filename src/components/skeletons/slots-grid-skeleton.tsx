e48df7d91271e3307bc98a7986a2578660d494d7
ShinjiCodeEVA <solisharvie1@gmail.com>
2025-11-05 21:57:03 +0800

    fix: display slot grid skeleton appropriately in large screen size

---
+++ src/components/skeletons/slots-grid-skeleton.tsx
@@ -0,0 +1,20 @@
+import { Skeleton } from "../ui/skeleton";
+
+interface SlotsGridSkeletonProps {
+  placeholderCount: number;
+}
+
+export default function SlotsGridSkeleton({
+  placeholderCount,
+}: SlotsGridSkeletonProps) {
+  return (
+    <div className="grid min-h-[12rem] animate-pulse grid-cols-2 gap-3 sm:grid-cols-3 lg:min-w-[585px] lg:grid-cols-4">
+      {Array.from({ length: placeholderCount }).map((_, i) => (
+        <Skeleton
+          key={i}
+          className="h-24 w-48 rounded-xl bg-gray-200 md:h-28 lg:w-34"
+        />
+      ))}
+    </div>
+  );
+}
