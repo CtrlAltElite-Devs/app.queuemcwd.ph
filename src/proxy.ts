0ff138d1ffb53d8bec6afe55f16988f1404f0446
Aya <ayacoders@gmail.com>
2025-11-15 21:57:05 +0800

    refactor: renamed middleware -> proxy as middleware is deprecated

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { redirectToLogin, validateToken } from "./services/validate-token";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const cookies = request.cookies.getAll();
 
   // Debug
